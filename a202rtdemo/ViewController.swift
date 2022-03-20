dce13a8197901a3524822638fa279bba8f1d1662
Danny Shen <shenfive@gmail.com>
2022-03-20 14:27:13 +0800

    訂閱

--- a202rtdemo/ViewController.swift
+++ a202rtdemo/ViewController.swift
@@ -22,9 +22,14 @@ class ViewController: UIViewController {
         
         ref = Database.database().reference()
         
-        ref.child("appStatus/ver").observeSingleEvent(of: .value) { snapshot in
-            print("Data is : \(snapshot.value as! Int)")
+//        ref.child("appStatus/ver").observeSingleEvent(of: .value) { snapshot in
+//            print("Data is : \(snapshot.value as! Int)")
+//        }
+        ref.child("appStatus").observe(.value) { dataSnapshot in
+            print("dataChanged")
+            print(dataSnapshot)
         }
+        
         
         ref.child("appStatus/setting/name").setValue("Danny Test 2")
         
